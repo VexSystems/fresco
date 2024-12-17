320ce657e98ddafa11c3dd4f565d66f3e3021208
velimirpaleksic <velimir.paleksic@gmail.com>
2024-12-17 03:00:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# La Delicia
+
+### Uživo Website 👀
+
+- ### [Live Website](https://velimirpaleksic.github.io/fresco/)
+
+### Greške & nadogradnje 🧩
+- [Prijavi grešku](https://github.com/velimirpaleksic/fresco/issues)
+- [Zatraži nadogradnju](https://github.com/velimirpaleksic/fresco/issues)
+
+### Sekcija 📚
+- Dobro došli
+- Istaknuta jela
+- Radno osoblje
+- Recenzije
+- O nama
+- Meni
+- Rezervacija
+- Galerija
+- Kontakt informacije
---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+function heroChange() {
+    const heroImages = document.querySelectorAll('.hero-image');
+    let currentImageIndex = 0;
+
+    // Function to fade images
+    function fadeImages() {
+        heroImages[currentImageIndex].style.opacity = 0;
+        currentImageIndex = (currentImageIndex + 1) % heroImages.length;
+        heroImages[currentImageIndex].style.opacity = 1;
+    }
+
+    // Call the fadeImages
+    setInterval(fadeImages, 3000);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = tns({
+        container: '.slider',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayButtonOutput: false,
+        controls: false,
+        nav: false,
+        speed: 500,
+    });
+});
